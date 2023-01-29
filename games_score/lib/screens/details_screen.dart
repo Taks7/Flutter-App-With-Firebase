96d082e5aa591a76ed091c64a19e70c9e7f762c5
Taks7 <albramisaplay@gmail.com>
2023-01-29 19:04:57 +0100

    Added more categories to rate in the Detail Screen

--- games_score/lib/screens/details_screen.dart
+++ games_score/lib/screens/details_screen.dart
@@ -26,160 +26,261 @@ class _details_screenState extends State<details_screen> {
   Widget build(BuildContext context) {
     final _db = FirebaseFirestore.instance;
     return Scaffold(
-      appBar: AppBar(title: Text("Rate  " + titleGame)),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Gameplay:  ",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              Container(
-                padding: const EdgeInsets.all(6),
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        title: Text(
+          "Rate:  " + titleGame,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Gameplay:  ",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
-                child: _ScoreAGame(
-                  sendRating: (String totalrating) {
-                    _db.collection("/${user.email!}/$titleGame/gameplay").add(
-                      {
-                        'rate': totalrating,
-                        'date': Timestamp.now(),
-                      },
-                    );
-                  },
+                Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: _ScoreAGame(
+                    sendRating: (String totalrating) {
+                      _db.collection("/${user.email!}/$titleGame/gameplay").add(
+                        {
+                          'rate': totalrating,
+                          'date': Timestamp.now(),
+                        },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Art:  ",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              Container(
-                padding: const EdgeInsets.all(6),
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Art:  ",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
-                child: _ScoreAGame(
-                  sendRating: (String totalrating) {
-                    _db.collection("/${user.email!}/$titleGame/art").add(
-                      {
-                        'rate': totalrating,
-                        'date': Timestamp.now(),
-                      },
-                    );
-                  },
+                Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: _ScoreAGame(
+                    sendRating: (String totalrating) {
+                      _db.collection("/${user.email!}/$titleGame/art").add(
+                        {
+                          'rate': totalrating,
+                          'date': Timestamp.now(),
+                        },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Music:  ",
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              Container(
-                padding: const EdgeInsets.all(6),
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(20)),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Music:  ",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
-                child: _ScoreAGame(
-                  sendRating: (String totalrating) {
-                    _db.collection("/${user.email!}/$titleGame/music").add(
-                      {
-                        'rate': totalrating,
-                        'date': Timestamp.now(),
-                      },
-                    );
-                  },
+                Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: _ScoreAGame(
+                    sendRating: (String totalrating) {
+                      _db.collection("/${user.email!}/$titleGame/music").add(
+                        {
+                          'rate': totalrating,
+                          'date': Timestamp.now(),
+                        },
+                      );
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Your last Gameplay rate was:  ",
-                style: TextStyle(
-                  fontSize: 16,
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Price:  ",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
-              ),
-              PrintLastRateGameplay(),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Your last Art rate was:  ",
-                style: TextStyle(
-                  fontSize: 16,
+                Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: _ScoreAGame(
+                    sendRating: (String totalrating) {
+                      _db.collection("/${user.email!}/$titleGame/price").add(
+                        {
+                          'rate': totalrating,
+                          'date': Timestamp.now(),
+                        },
+                      );
+                    },
+                  ),
                 ),
-              ),
-              PrintLastRateArt(),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Your last Music rate was:  ",
-                style: TextStyle(
-                  fontSize: 16,
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Replayability:  ",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
-              ),
-              PrintLastRateMusic(),
-            ],
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "WIP: More categories will be added in a future",
-                style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              )
-            ],
-          )
-        ],
+                Container(
+                  padding: const EdgeInsets.all(6),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  ),
+                  child: _ScoreAGame(
+                    sendRating: (String totalrating) {
+                      _db
+                          .collection(
+                              "/${user.email!}/$titleGame/replayability")
+                          .add(
+                        {
+                          'rate': totalrating,
+                          'date': Timestamp.now(),
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your last Gameplay rate was:  ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                PrintLastRateGameplay(),
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your last Art rate was:  ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                PrintLastRateArt(),
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your last Music rate was:  ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                PrintLastRateMusic(),
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your last Price rate was:  ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                PrintLastRatePrice(),
+              ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your last Replayability rate was:  ",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                PrintLastRateReplayability(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -293,6 +394,108 @@ class PrintLastRateMusic extends StatelessWidget {
   Widget build(BuildContext context) {
     final _db = FirebaseFirestore.instance;
     final ratingPath = "/${user.email!}/$titleGame/music";
+    return StreamBuilder(
+      stream: _db
+          .collection(ratingPath)
+          .orderBy("date", descending: true)
+          .snapshots(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
+      ) {
+        final querySnap = snapshot.data!;
+        final docs = querySnap.docs;
+
+        if (docs.length == 0) {
+          //Comprobem si hi ha algo dins de la llista
+          return Text(
+            "There is no last review",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold,
+            ),
+          );
+        } else {
+          final rating = docs[0];
+          return Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                ),
+                child: Text(
+                  rating['rate'],
+                  style: TextStyle(color: Colors.yellow, fontSize: 20),
+                ),
+              ),
+            ],
+          );
+        }
+      },
+    );
+  }
+}
+
+class PrintLastRatePrice extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser!;
+  @override
+  Widget build(BuildContext context) {
+    final _db = FirebaseFirestore.instance;
+    final ratingPath = "/${user.email!}/$titleGame/price";
+    return StreamBuilder(
+      stream: _db
+          .collection(ratingPath)
+          .orderBy("date", descending: true)
+          .snapshots(),
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot,
+      ) {
+        final querySnap = snapshot.data!;
+        final docs = querySnap.docs;
+
+        if (docs.length == 0) {
+          //Comprobem si hi ha algo dins de la llista
+          return Text(
+            "There is no last review",
+            style: TextStyle(
+              fontSize: 15,
+              color: Colors.blueGrey,
+              fontWeight: FontWeight.bold,
+            ),
+          );
+        } else {
+          final rating = docs[0];
+          return Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                ),
+                child: Text(
+                  rating['rate'],
+                  style: TextStyle(color: Colors.yellow, fontSize: 20),
+                ),
+              ),
+            ],
+          );
+        }
+      },
+    );
+  }
+}
+
+class PrintLastRateReplayability extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser!;
+  @override
+  Widget build(BuildContext context) {
+    final _db = FirebaseFirestore.instance;
+    final ratingPath = "/${user.email!}/$titleGame/replayability";
     return StreamBuilder(
       stream: _db
           .collection(ratingPath)
