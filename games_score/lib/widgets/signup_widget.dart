3f380b18d05810add20f87518dac88e9d0b1670f
Taks7 <albramisaplay@gmail.com>
2023-01-29 18:07:10 +0100

    When signing Up the emails and passwords are validated
    Done with the email_validator package

--- games_score/lib/widgets/signup_widget.dart
+++ games_score/lib/widgets/signup_widget.dart
@@ -1,6 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
+
+// We have used https://pub.dev/packages/email_validator
+//To validate the emails introduced in the Sign Up process
 
 class SignUpWidget extends StatefulWidget {
   final Function() onClickedSignIn;
@@ -15,6 +19,7 @@ class SignUpWidget extends StatefulWidget {
 }
 
 class _SignUpWidgetState extends State<SignUpWidget> {
+  final formKey = GlobalKey<FormState>();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -29,76 +34,91 @@ class _SignUpWidgetState extends State<SignUpWidget> {
   @override
   Widget build(BuildContext context) => SingleChildScrollView(
         padding: EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 60,
-            ),
-            FlutterLogo(
-              size: 120,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Welcome to the \n Game Searcher!",
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            TextField(
-              controller: emailController,
-              cursorColor: Colors.amber,
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(labelText: "Email"),
-              obscureText: true,
-            ),
-            SizedBox(height: 6),
-            TextField(
-              controller: passwordController,
-              cursorColor: Colors.amber,
-              textInputAction: TextInputAction.done,
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size.fromHeight(50),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 60,
               ),
-              icon: Icon(
-                Icons.lock_outline,
-                size: 32,
+              FlutterLogo(
+                size: 120,
               ),
-              label: Text(
-                "Sign Up",
-                style: TextStyle(fontSize: 24),
+              SizedBox(
+                height: 20,
               ),
-              onPressed: signUp,
-            ),
-            SizedBox(
-              height: 24,
-            ),
-            RichText(
-                text: TextSpan(
-                    style: TextStyle(color: Colors.black, fontSize: 20),
-                    text: "Already have an account? ",
-                    children: [
-                  TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = widget.onClickedSignIn,
-                      text: "Log In",
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Theme.of(context).colorScheme.secondary)),
-                ]))
-          ],
+              Text(
+                "Welcome to the \n Game Searcher!",
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextFormField(
+                controller: emailController,
+                cursorColor: Colors.amber,
+                textInputAction: TextInputAction.next,
+                decoration: InputDecoration(labelText: "Email"),
+                obscureText: true,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? "Enter a Valid Email"
+                        : null,
+              ),
+              SizedBox(height: 6),
+              TextFormField(
+                controller: passwordController,
+                cursorColor: Colors.amber,
+                textInputAction: TextInputAction.done,
+                decoration: InputDecoration(labelText: "Password"),
+                obscureText: true,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) => value != null && value.length < 6
+                    ? "Enter a minimum of 6 characters"
+                    : null,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size.fromHeight(50),
+                ),
+                icon: Icon(
+                  Icons.lock_outline,
+                  size: 32,
+                ),
+                label: Text(
+                  "Sign Up",
+                  style: TextStyle(fontSize: 24),
+                ),
+                onPressed: signUp,
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              RichText(
+                  text: TextSpan(
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                      text: "Already have an account? ",
+                      children: [
+                    TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = widget.onClickedSignIn,
+                        text: "Log In",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Theme.of(context).colorScheme.secondary)),
+                  ]))
+            ],
+          ),
         ),
       );
 
   Future signUp() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
     await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: emailController.text.trim(),
         password: passwordController.text.trim());
